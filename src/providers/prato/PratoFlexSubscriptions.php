1b6123974cb07954f334bab728c28701472a2646
Michael Thomas <michael@dadamotion.be>
2024-09-26 15:21:17 +0200

    feat: phone number normaliser for pratoflex

--- src/providers/prato/PratoFlexSubscriptions.php
+++ src/providers/prato/PratoFlexSubscriptions.php
@@ -29,6 +29,193 @@ class PratoFlexSubscriptions extends Component
      */
     const GO4JOBS = 2;
 
+    const COUNTRYCODES = [
+        '1',    // USA, Canada, etc.
+        '20',   // Egypt
+        '30',   // Greece
+        '31',   // Netherlands
+        '32',   // Belgium
+        '33',   // France
+        '34',   // Spain
+        '39',   // Italy
+        '44',   // UK
+        '49',   // Germany
+        '52',   // Mexico
+        '55',   // Brazil
+        '61',   // Australia
+        '62',   // Indonesia
+        '63',   // Philippines
+        '64',   // New Zealand
+        '65',   // Singapore
+        '66',   // Thailand
+        '81',   // Japan
+        '82',   // South Korea
+        '86',   // China
+        '91',   // India
+        '92',   // Pakistan
+        '93',   // Afghanistan
+        '94',   // Sri Lanka
+        '98',   // Iran
+        '211',  // South Sudan
+        '212',  // Morocco
+        '213',  // Algeria
+        '216',  // Tunisia
+        '218',  // Libya
+        '220',  // Gambia
+        '221',  // Senegal
+        '222',  // Mauritania
+        '223',  // Mali
+        '224',  // Guinea
+        '225',  // Ivory Coast
+        '226',  // Burkina Faso
+        '227',  // Niger
+        '228',  // Togo
+        '229',  // Benin
+        '230',  // Mauritius
+        '231',  // Liberia
+        '232',  // Sierra Leone
+        '233',  // Ghana
+        '234',  // Nigeria
+        '235',  // Chad
+        '236',  // Central African Republic
+        '237',  // Cameroon
+        '238',  // Cape Verde
+        '239',  // São Tomé and Príncipe
+        '240',  // Equatorial Guinea
+        '241',  // Gabon
+        '242',  // Congo
+        '243',  // Democratic Republic of the Congo
+        '244',  // Angola
+        '245',  // Guinea-Bissau
+        '246',  // British Indian Ocean Territory
+        '247',  // Ascension
+        '248',  // Seychelles
+        '249',  // Sudan
+        '250',  // Rwanda
+        '251',  // Ethiopia
+        '252',  // Somalia
+        '253',  // Djibouti
+        '254',  // Kenya
+        '255',  // Tanzania
+        '256',  // Uganda
+        '257',  // Burundi
+        '258',  // Mozambique
+        '260',  // Zambia
+        '261',  // Madagascar
+        '262',  // Reunion (France)
+        '263',  // Zimbabwe
+        '264',  // Namibia
+        '265',  // Malawi
+        '266',  // Lesotho
+        '267',  // Botswana
+        '268',  // Swaziland
+        '269',  // Comoros
+        '290',  // Saint Helena
+        '291',  // Eritrea
+        '297',  // Aruba
+        '298',  // Faroe Islands
+        '299',  // Greenland
+        '350',  // Gibraltar
+        '351',  // Portugal
+        '352',  // Luxembourg
+        '353',  // Ireland
+        '354',  // Iceland
+        '355',  // Albania
+        '356',  // Malta
+        '357',  // Cyprus
+        '358',  // Finland
+        '359',  // Bulgaria
+        '370',  // Lithuania
+        '371',  // Latvia
+        '372',  // Estonia
+        '373',  // Moldova
+        '374',  // Armenia
+        '375',  // Belarus
+        '376',  // Andorra
+        '377',  // Monaco
+        '378',  // San Marino
+        '380',  // Ukraine
+        '381',  // Serbia
+        '382',  // Montenegro
+        '383',  // Kosovo
+        '385',  // Croatia
+        '386',  // Slovenia
+        '387',  // Bosnia and Herzegovina
+        '389',  // North Macedonia
+        '420',  // Czech Republic
+        '421',  // Slovakia
+        '423',  // Liechtenstein
+        '500',  // Falkland Islands
+        '501',  // Belize
+        '502',  // Guatemala
+        '503',  // El Salvador
+        '504',  // Honduras
+        '505',  // Nicaragua
+        '506',  // Costa Rica
+        '507',  // Panama
+        '508',  // Saint Pierre and Miquelon
+        '509',  // Haiti
+        '590',  // Guadeloupe
+        '591',  // Bolivia
+        '592',  // Guyana
+        '593',  // Ecuador
+        '594',  // French Guiana
+        '595',  // Paraguay
+        '596',  // Martinique
+        '597',  // Suriname
+        '598',  // Uruguay
+        '670',  // East Timor
+        '672',  // Antarctica and some Australian territories
+        '673',  // Brunei
+        '674',  // Nauru
+        '675',  // Papua New Guinea
+        '676',  // Tonga
+        '677',  // Solomon Islands
+        '678',  // Vanuatu
+        '679',  // Fiji
+        '680',  // Palau
+        '681',  // Wallis and Futuna
+        '682',  // Cook Islands
+        '683',  // Niue
+        '685',  // Samoa
+        '686',  // Kiribati
+        '687',  // New Caledonia
+        '688',  // Tuvalu
+        '689',  // French Polynesia
+        '690',  // Tokelau
+        '691',  // Micronesia
+        '692',  // Marshall Islands
+        '850',  // North Korea
+        '852',  // Hong Kong
+        '853',  // Macau
+        '855',  // Cambodia
+        '856',  // Laos
+        '880',  // Bangladesh
+        '886',  // Taiwan
+        '960',  // Maldives
+        '961',  // Lebanon
+        '962',  // Jordan
+        '963',  // Syria
+        '964',  // Iraq
+        '965',  // Kuwait
+        '966',  // Saudi Arabia
+        '967',  // Yemen
+        '968',  // Oman
+        '971',  // United Arab Emirates
+        '972',  // Israel
+        '973',  // Bahrain
+        '974',  // Qatar
+        '975',  // Bhutan
+        '976',  // Mongolia
+        '977',  // Nepal
+        '992',  // Tajikistan
+        '993',  // Turkmenistan
+        '994',  // Azerbaijan
+        '995',  // Georgia
+        '996',  // Kyrgyzstan
+        '998',  // Uzbekistan
+    ];
+
     /**
      * @param Submission $submission
      * @return void
@@ -263,7 +450,7 @@ class PratoFlexSubscriptions extends Component
             'office' => $office->branchId,
             'inss' => (string) $submission->inss,
             'info' => (string) $submission->about,
-            'mobile' => (string) $submission->phone,
+            'mobile' => $this->_formatPhoneNumber($submission->phone),
             'language' => 1,
             'recruitmentchannel' => self::GO4JOBS,
         ];
@@ -284,5 +471,30 @@ class PratoFlexSubscriptions extends Component
             // Let's add the CV to the user
             Ats::$plugin->pratoProvider->pushCvToUser($office, $pratoUser, $cvFile);
         }
+    }
+
+    private function _formatPhoneNumber(string $phone): string
+    {
+        // Remove all spaces, dashes, slashes, underscores, and non-digit characters except '+'.
+        $normalizedNumber = preg_replace('/[^\d+]/', '', $phone);
+
+        // If the number starts with a '+', replace it with '00'
+        if (str_starts_with($normalizedNumber, '+')) {
+            $normalizedNumber = '00' . substr($normalizedNumber, 1); // Replace '+' with '00'
+        }
+
+        // Check the country code against our list of known country codes (1 to 3 digits)
+        foreach (self::COUNTRYCODES as $code) {
+            if (str_starts_with($normalizedNumber, $code)) {
+                $countryCode = $code;
+                $localNumber = substr($normalizedNumber, strlen($countryCode));
+
+                // Format as (countryCode)localNumber
+                return "({$countryCode}){$localNumber}";
+            }
+        }
+
+        // Return the original number if it doesn't match the expected pattern
+        return $phone;
     }
 }
